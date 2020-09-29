ee5ca02c8c3561bdf973f899646f358bc15a811b
kuzenka <63797645+kuzenka@users.noreply.github.com>
2020-09-29 19:19:53 +0300

    Add files via upload

---
+++ project 5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <Book.h>
+#include <PrintBook.h>
+#include <shelf.h>
+#include <Bshelf.h>
+using namespace std
+
+int main{int args,char}
+    system (chep 1251) // кодировка win=1251
+    // режим работы
+    int mode =-1
+    // Шкафы
+    vector<BookShelf> Bshelf
+    BookShelf Bshelf //Пустой шкаф
+    while (mode=0) {//mode = 0 - Выход
+     cout<<" МЕНЮ " <<end1;
+     cout<<" 1.Добавить шкаф. " <<end1;
+     cout<<" 2. Добавить полку в шкаф " <<end1;
+	 cout<<" 3. Добавить книгу.  " <<end1;
+     cout<<" 4. Вывод на экран " <<end1;
+     cout<<" 0. Выход " <<end1;
+     cout<<"Введите номер и нажмите <ENTER> "
+     cin>Mode
+     switch(mode)
+        case 0: countinue;
+        case 1: //Добавить шкаф
+        
+            cout <<"доступно">>
+            cout <<"введите номер шкафа">>
+            int n=0
+            cin>>n
+            if (n>0) // проверка на допустимость значения
+            Bshelfs[n=1] addshelf
+            cout<<//полка добавлена
+        else{
+        	cout<<"ошибка в номере шкафа. Повторите
+        	
+	else{
+		cout<<"доступные шкафы отсутствуют"
+	
+	}		    	
+		}
+        break
+	case //Добавить книгу
+	if //проверка на наличие шкафов
+		cout<<"доступно" 
+		cout<<"Введите номер шкафа"   
+		int n=0
+		cin>>n
+		PrintBook // Пустая книга
+		cout<<"введите название книги"
+		string Name
+		cin>> Name
+		cout <<"Введите кол-во страниц"
+		
+		    cout <<"доступные шкафы отсутствуют"
+		    
+		    
+		}
+		Break
+	case 4:// Вывод на экран
+	    For (int=0)
+	        Cout <<"Шкаф"
+	        bshelfs[i] Print()
+	    }
+	    cout<<end1;
+	break
+    }
+}
+    Cout<<"Пока"
+    return 0
+
